1fd05fc0172be1d21ce5d7686acb7e2e42d0e9da
Harshita <harshita0601.be23@chitkara.edu.in>
2024-12-11 22:13:42 +0530

    done

---
+++ CODE/Function.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Function {
+    // public static void main(String[] args)
+    // {
+    // int a=20;
+    // int b=10;
+    // int ans=sum(a,b);
+    // System.out.println(ans);
+    // }
+    // static int sum(int a, int b)
+    // {
+    // int sum_num=a+b;
+    // return sum_num;
+    // }
+    // public static void printMyName(String name) {
+    //     System.out.println(name);
+    //     return;
+    // }
+    // public static int multiplyNum(int a,int b)
+    // {
+    //     int mul_num=a*b;
+    //     return mul_num;
+    // }
+    // public static void fact(int num)
+    // {
+    //     if (num < 0) {
+    //         System.out.println("Factorial is not defined for negative numbers.");
+    //         return;
+    //     }
+    //     int factorial=1;
+    //     for(int i=1;i<=num;i++)
+    //     {
+    //         factorial=factorial*i;
+    //         System.out.println(factorial);
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int ans=multiplyNum(a, b);
+        // System.out.println(ans);
+        // String name = sc.next();
+        // printMyName(name);
+        // fact(a);
+        sc.close();
+    }
+}
