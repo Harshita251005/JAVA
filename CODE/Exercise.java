76f45d0eac66ac3239e86474c154ce35f465d1c3
Harshita <harshita0601.be23@chitkara.edu.in>
2024-12-12 12:09:54 +0530

    done

---
+++ CODE/Exercise.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Exercise {
+    // public static int avg(int n1,int n2,int n3)
+    // {
+    // int avg=(n1+n2+n3)/3;
+    // System.out.println(avg);
+    // return avg;
+    // }
+    // public static void odd(int n)
+    // {
+    // int sum=0;
+    // for(int i=1;i<=n;i=i+2)
+    // {
+    // sum=sum+i;
+    // }
+    // System.out.println(sum);
+    // }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int num1=sc.nextInt();
+        // int num2=sc.nextInt();
+        // float r=sc.nextFloat();
+        // int num3=sc.nextInt();
+        // avg(num1,num2,num3);
+        // int num=sc.nextInt();
+        // odd(num);
+        // if(num1>num2)
+        // {
+        // System.out.println("num1 is greatest");
+        // }
+        // else{
+        // System.out.println("num2 is greatest");
+        // }
+        // float circum=2*(float)Math.PI*r;
+        // System.out.println(circum);
+        // if(num1>18)
+        // {
+        // System.out.println("Eligible to Vote");
+        // }
+        // else{
+        // System.out.println("Not Eligible to vote");
+        // }
+        // int i=num1;
+        // while(true)
+        // {
+        // System.out.println(num1);
+        // i++;
+        // }
+        // if(num1>0)
+        // {
+        // System.out.println("Positive");
+        // }
+        // else if(num1<0)
+        // {
+        // System.out.println("Negative");
+        // }
+        // else
+        // {
+        // System.out.println("Zero");
+        // }
+        // int pow=(int) Math.pow(num1,num2);
+        // System.out.println(pow);
+
+
+        // int n = sc.nextInt();
+        // int n1=sc.nextInt();
+        // while(n!=n1)
+        // {
+        //     if (n>n1) {
+        //         n=n-n1;
+        //     }
+        //     else{
+        //         n1=n1-n;
+        //     }
+        // }
+        // System.out.println("GCd is " + n1);
+
+        // int n1 = 0;
+        // int n2 = 1;
+        // System.out.print(n1 + " " + n2 + " ");
+        // for (int i = 3; i <= n; i++) {
+        //     int sum = n1 + n2;
+        //     System.out.print(sum + " ");
+        //     n1 = n2;
+        //     n2 = sum;
+        // }
+
+        sc.close();
+    }
+
+}
