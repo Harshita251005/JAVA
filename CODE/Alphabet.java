a77db0cc350abc8e6c28c26439bc124cb8eb1cc3
Harshita <harshita0601.be23@chitkara.edu.in>
2025-01-20 11:28:25 +0530

    done

---
+++ CODE/Alphabet.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Alphabet {
+    public static void main(String arg[]) {
+        // String Decleration
+        String name = "Tony";
+        String fullname = "Tony Stark";
+        String sentence = "My name is Tony Stark";
+        System.out.println(name);
+        System.out.println(fullname);
+        System.out.println(sentence);
+        Scanner sc = new Scanner(System.in);
+        String fname = sc.nextLine();
+        System.out.println(fname);
+
+    }
+}
+
--- CODE/Class.java
+++ CODE/Class.java
@@ -1,9 +1,7 @@
-
-
-public class Class {
-    public static void main(String args[])
-    {
-        // System.out.print("Harshita");
-        System.out.print(args[0]);
-    }
-}
+// public class Class {
+//     public static void main(String args[])
+//     {
+//         System.out.print("Harshita");
+//         System.out.print(args[0]);
+//     }
+// }
