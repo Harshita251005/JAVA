77f1f1b9ece5e470b9d8b4e35578f8253ca6996c
Harshita <harshita0601.be23@chitkara.edu.in>
2025-01-31 20:24:33 +0530

    done

--- CODE/Class.java
+++ CODE/Class.java
@@ -1,7 +1,11 @@
-// public class Class {
-//     public static void main(String args[])
-//     {
-//         System.out.print("Harshita");
-//         System.out.print(args[0]);
-//     }
-// }
+public class Class {
+    public static void main(String args[]) {
+        // System.out.print("Harshita");
+        // System.out.print(args[0]);
+        int number = 4;
+        for (int count = 1; count <= number; count++) {
+            System.out.println(number);
+            number = number / 2;
+        }
+    }
+}
