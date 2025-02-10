eb95b80f202a4c4f7da310028d902e02a031fe1c
Harshita <harshita0601.be23@chitkara.edu.in>
2025-02-10 21:45:02 +0530

    done

--- CODE/Opps.java
+++ CODE/Opps.java
@@ -794,3 +794,58 @@
 //         im.print();
 //     }
 // }
+
+
+// public class Class29 {
+//     public static void main(String[] args) {
+// //        Scanner sc = new Scanner(System.in);
+// //        meow124 obj = new meow124();
+// //        obj.a = 12;
+//         //abstraction->
+//         //process of hiding complex implementation from user.
+//         //showing functionality to user.
+//         meow126 h = new meow126();
+//         h.hey();
+//     }
+// }
+
+
+// package Lec_29_Feb_10_OOPs;
+
+// abstract class MotorVehicle{
+//     String name;
+//     abstract void start();
+//     // abstract private void abstract_method();    // abstract method can't be private
+//     // abstract static void static_method();    // abstract method can't be static
+//     // abstract final void final_method();    // abstract method can't be final
+//     void print(){
+//         System.out.println("I'm Abstract Class");
+//     }
+//     // other functionality can be same
+//     static void static_non_abstract_method(){
+
+//     }
+//     MotorVehicle(){
+//         System.out.println("Motor Vehicle");
+//         this.name = "Abc";
+//     }
+// }
+
+// abstract class Car extends MotorVehicle{
+//     // class must be ebstract because 
+//     // it is not implementing abstract methods in parent class
+// }
+
+// class Bike extends MotorVehicle{
+//     void start(){
+//         System.out.println("Starts from Kick");
+//     }
+// }
+
+// public class abstraction {
+//     public static void main(String[] args) {
+        
+//         // cannot make objects of abtract class
+//         // MotorVehicle mv = new MotorVehicle();
+//     }
+// }
