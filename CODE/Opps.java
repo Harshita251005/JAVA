51fa36ab35d31e063c120c491c8b89b694f4d7ff
Harshita <harshita0601.be23@chitkara.edu.in>
2025-02-25 19:44:43 +0530

    done

--- CODE/Opps.java
+++ CODE/Opps.java
@@ -1235,3 +1235,116 @@
 //         Collections.sort(arr, new SortByMarks());
 //     }
 // }
+
+
+
+
+
+// package Lec_37_Feb_25_OOPs;
+
+// interface generic_interface<T>{
+//     T getKey();
+//     void setKey(T key);
+// }
+// class example<T> implements generic_interface<T>{
+//     private T key;
+//     public T getKey(){
+//         return this.key;
+//     }
+//     public void setKey(T key){
+//         this.key = key;
+//     }
+// }
+
+// class key_val<K,V>{
+//     K key;
+//     V val;
+//     key_val(K key, V val){
+//         this.key = key;
+//         this.val = val;
+//     }
+//     K getKey(){
+//         return this.key;
+//     }
+// }
+
+// class normalClass{
+//     static <T> void print(T obj){
+//         System.out.println(obj);
+//     }
+// }
+
+// public class generics {
+//     public static void main(String[] args) {
+        
+//         key_val<String, Integer> obj1 = new key_val("Jai", 1);
+        
+//         normalClass.print("Jai");
+//         normalClass.print(3);
+//     }
+// }
+
+// package Lec_37_Feb_25_OOPs;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Collections;
+// import java.util.Comparator;
+// import java.util.Scanner;
+
+// class KeyValue{
+//     int key;
+//     int val;
+//     KeyValue(int key, int val){
+//         this.key = key;
+//         this.val = val;
+//     }
+//     @Override
+//     public String toString(){
+//         return ("Key : " + key + " || Value : " + val);
+//     }
+// }
+// // class SortByValue implements Comparator<KeyValue>{
+// //     @Override
+// //     public int compare(KeyValue kv1, KeyValue kv2){
+// //         return Integer.compare(kv2.val, kv1.val);
+// //     }
+// // }
+
+// public class question {
+//     public static void main(String[] args) {
+        
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//         ArrayList<KeyValue> arrList = new ArrayList<>();
+//         KeyValue[] arr = new KeyValue[n];
+
+//         for(int i=0 ; i<n ; i++){
+//             int key = sc.nextInt();
+//             int val = sc.nextInt();
+//             arr[i] = new KeyValue(key, val);
+//             arrList.add(new KeyValue(key, val));
+//         }
+
+//         // using Lamda expression
+//         Collections.sort(arrList, (kv1, kv2)->Integer.compare(kv2.val, kv1.val));
+//         arrList.sort((kv1, kv2)->Integer.compare(kv2.val, kv1.val));
+//         Arrays.sort(arr, (kv1, kv2)->Integer.compare(kv2.val, kv1.val));
+
+//         System.out.println("-------------------ArrayList---------------");
+//         for(KeyValue kv : arrList){
+//             System.out.println(kv);
+//         }
+//         System.out.println("-------------------Array---------------");
+//         for(KeyValue kv : arr){
+//             System.out.println(kv);
+//         }
+
+//         // using Anonymous class
+//         arrList.sort(new Comparator<KeyValue>() {
+//             public int compare(KeyValue kv1, KeyValue kv2){
+//                 return Integer.compare(kv2.val, kv1.val);
+//             }
+//         });
+//     }
+// }
