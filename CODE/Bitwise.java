493ef880464c6d0f8e09f21770d490adb0a7b276
Harshita <harshita0601.be23@chitkara.edu.in>
2025-03-17 22:04:38 +0530

    done

--- CODE/Bitwise.java
+++ CODE/Bitwise.java
@@ -3,11 +3,11 @@ import java.util.*;
 public class Bitwise {
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
-        int oper  = sc.nextInt();
-        int n = 5; // 0101
-        // int pos=2;
-        int pos = 1;
-        int bitMask = 1 << pos;
+        // int oper  = sc.nextInt();
+        // int n = 5; // 0101
+        // // int pos=2;
+        // int pos = 1;
+        // int bitMask = 1 << pos;
         // int notBitMask = ~(bitMask);
         // if((bitMask & n)==0){
         // System.out.println("bit was zero");
@@ -18,18 +18,31 @@ public class Bitwise {
         // int newNumber = bitMask | n;
         // int newNumber = notBitMask & n;
         // System.out.println(newNumber);
-        if (oper == 1)
-        {
-            //set
-            int newNumber = bitMask | n;
-            System.out.println(newNumber);
-        }
-        else{
-            //clear
-            int newBitMask = ~(bitMask);
-            int newNumber = newBitMask & n;
-            System.out.println(newNumber);
-        }
+        // if (oper == 1)
+        // {
+        //     //set
+        //     int newNumber = bitMask | n;
+        //     System.out.println(newNumber);
+        // }
+        // else{
+        //     //clear
+        //     int newBitMask = ~(bitMask);
+        //     int newNumber = newBitMask & n;
+        //     System.out.println(newNumber);
+        // }
+        // int num = sc.nextInt();
+        // if((num & 1)==1)
+        // {
+        //     System.out.println("Odd");
+        // }
+        // else{
+        //     System.out.println("Even");
+        // }
+        // if((num &(num-1))==0)
+        // {
+        //     System.out.println("Power of 2");
+        // }
+        // else System.out.println("Not");
         sc.close();
     }
 }
