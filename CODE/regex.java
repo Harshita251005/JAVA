f17c9647f7caa89cc2da90e6cd738d01b9d15f68
Harshita <harshita0601.be23@chitkara.edu.in>
2025-04-24 15:01:29 +0530

    done

--- CODE/regex.java
+++ CODE/regex.java
@@ -111,3 +111,196 @@
 //         System.out.println(removed_comment);
 //     }
 // }
+
+
+
+// package Lec_52_April_17_Regex;
+
+// import java.util.Scanner;
+// import java.util.regex.Matcher;
+// import java.util.regex.Pattern;
+
+// public class words_with_first_caps {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String str = sc.nextLine();
+
+//         String reg = "\\b[A-Z]\\w*\\b";
+//         Pattern p = Pattern.compile(reg);
+//         Matcher m = p.matcher(str);
+
+//         while(m.find()){
+//             System.out.println(m.group());
+//         }
+//     }
+// }
+
+
+
+
+// package Lec_53_April_21_Enums;
+
+// enum Day{
+//     Monday, Tuesday, Wednesday, Tursday, Friday, Saturday, Sunday;
+// }
+
+// enum STATUS{
+//     Success(200, "success"),
+//     NotFound(404, "Please hit a valid URL"),
+//     TimeOut(501, "Taking too long to respond, Try again later");
+
+//     private int code;
+//     private String msg;
+
+//     private STATUS(int code, String msg){
+//         this.code = code;
+//         this.msg = msg;
+//     }
+//     public int getStatusCode(){
+//         return this.code;
+//     }
+//     public String getStatusMsg(){
+//         return this.msg;
+//     }
+// }
+
+// enum Maths{
+//     Add{
+//         public int operation(int a, int b){
+//             return a + b;
+//         }
+//     },
+//     Multiply{
+//         public int operation(int a, int b){
+//             return a * b;
+//         }
+//     };
+//     public abstract int operation(int a, int b);
+// }
+
+// public class enums {
+//     public static void main(String[] args) {
+        
+//         Day d = Day.Monday;
+
+//         switch (d) {
+//             case Monday:
+//                 System.out.println("Here we go again!!");
+//                 break;
+//             case Friday:
+//                 System.out.println("Weekend starts !!");
+//                 break;
+//             default:
+//                 break;
+//         }
+
+//         STATUS urlResponse = STATUS.NotFound;
+//         System.out.println(urlResponse.getStatusCode() + " : " + urlResponse.getStatusMsg());
+
+//         for(STATUS s : STATUS.values()){
+//             System.out.println(s + " || " + s.getStatusCode() + " : " + s.getStatusMsg());
+//         }
+
+//         int sum = Maths.Add.operation(10, 20);
+//         // OR
+//         Maths m = Maths.Add;
+//         sum = m.operation(10, 20);
+//     }
+// }
+
+
+
+// package Lec_54_April_22_Random_DateTime;
+
+// import java.util.Random;
+
+// public class random {
+//     public static void main(String[] args) {
+        
+//         Random random = new Random();
+
+//         int num = random.nextInt();
+
+//         // in range [0,99]
+//         num = random.nextInt(100);
+
+//         // generate 10 random numbers in range [0,100]
+//         for(int i=0 ; i<10 ; i++){
+//             System.out.println(random.nextInt(101));
+//         }
+
+//         // Generate random number in range [50,100]
+//         num = random.nextInt(50,101);   // in Java 17 and above
+//         num = random.nextInt(51) + 50;  // in Java <17
+
+//         // Generate 20 random numbers in range [-100,100]
+//         for(int i=0 ; i<20 ; i++){
+//             System.out.println(random.nextInt(-100, 101));  // Java >=17
+//             System.out.println((random.nextInt(201) - 100));    // java <17
+//         }
+
+//         // Generate a 6 digit OTP
+//         int OTP = random.nextInt(100000, 1000000);
+
+//         // Generate a 6 digit OTP without 0
+//         OTP = 0;
+//         for(int i=0 ; i<6 ; i++){
+//             OTP = OTP * 10;
+//             OTP = OTP + random.nextInt(9) + 1;
+//         }
+//         System.out.println(OTP);
+//     }
+// }
+
+
+
+// package Lec_55_April_23_DateTime;
+// import java.time.*;
+
+// public class datetime {
+//     public static void main(String[] args) {
+        
+//         LocalDate today = LocalDate.now();
+//         LocalTime currTime = LocalTime.now();
+//         LocalDateTime dateTime = LocalDateTime.now();
+
+//         System.out.println("Date : " + today + " || Time : " + currTime);
+
+//         LocalDate customDate = LocalDate.of(2011, 04, 02);
+//         LocalTime customTime = LocalTime.of(23, 30);
+
+//         // comparing dates of time - isBefore() / isAfter()
+//         System.out.println(customDate.isBefore(today));
+//         System.out.println(currTime.isAfter(customTime));
+
+//         // getting day, month, year from LocalDate, LocalDateTime
+//         int day = today.getDayOfMonth();
+//         Month month = today.getMonth();
+//         int monthCount = dateTime.getMonthValue();
+
+//         // getting hour, min, second from LocalTime, LocalDateTime
+//         int hour = currTime.getHour();
+//         int min = dateTime.getMinute();
+//         int second = currTime.getSecond();
+
+//         // convert to LocalDate / LocalTime from LocalDateTime
+//         LocalDate date = dateTime.toLocalDate();
+//         LocalTime time = dateTime.toLocalTime();
+
+//         // Calculate previous date / time
+//         LocalDateTime previousDateTime = LocalDateTime.now().minusDays(12);
+//         previousDateTime = LocalDateTime.now().minusMonths(18);
+//         previousDateTime = LocalDateTime.now().minusHours(62);
+
+//         // calulate difference in dates
+//         Period p = Period.between(customDate, today);
+//         Duration hourDuration = Duration.between(customTime, dateTime);
+
+//         // ZoneDateTime
+//         ZonedDateTime IndianTime = ZonedDateTime.now();
+//         System.out.println(IndianTime);
+        
+//         ZonedDateTime LondonTime = ZonedDateTime.now(ZoneId.of("Europe/London"));
+//         System.out.println(LondonTime);
+//     }
+// }
